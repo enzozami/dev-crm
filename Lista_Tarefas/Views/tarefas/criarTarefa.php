c85865c61963a3b672d813ec36acbb8e28c52ae6
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-27 19:38:38 -0300

    feat(tarefas): Criar view criarTarefa.php

---
+++ Lista_Tarefas/Views/tarefas/criarTarefa.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Criar Nova Tarefa</title>
+    <link rel="stylesheet" href="../style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <?php
+        include_once "../layout/navBar.php";
+    ?>
+
+    <div class="container">
+        <form action="" method="post">
+            <h2 class="text-center my-5">NOVA TAREFA</h2>
+
+            <div class="border rounded p-3 m-2 text-center">
+                
+                <h4 class="text-center my-2">Criar Tarefa</h4>
+
+                <div class="row mt-3 p-2 text-center">
+                    <div class="col">
+                        <label for="titulo" class="text-center form-label">Título</label>
+                        <input type="text" name="titulo" class="form-control rounded-pill" style="text-transform: capitalize">
+                    </div>
+                    <div class="col">
+                        <label for="descricao" class="text-center form-label">Descrição</label>
+                        <input type="text" name="descricao" class="form-control rounded-pill" style="text-transform: capitalize">
+                    </div>
+                </div>
+                <div class="row mt-3 text-center">
+                    <div class="col">
+                        <label for="dataCriacao" class="text-center form-label">Data de Criação</label>
+                        <input type="date" name="dataCriacao" class="form-control rounded-pill">
+                    </div>
+                    <div class="col">
+                        <label for="usuario" class="text-center form-label">Usuário</label>
+                        <input type="text" name="usuario" class="form-control rounded-pill" style="text-transform: capitalize">
+                    </div>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-center align-items-center">
+                <button class="btn btn-outline-success my-2">Criar Tarefa</button>
+            </div>
+        </form>
+    </div>
+
+    <?php
+        include_once "../layout/footer.php";
+    ?>
+</body>
+</html>
