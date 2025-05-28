29ed18a49bf6528cd9e019a5954b9f55734e6164
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-28 10:00:22 -0300

    style: Adicionar style na pagina index.php

--- Lista_Tarefas/index.php
+++ Lista_Tarefas/index.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        input{
+            border-radius: 1rem !important;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
