531163a1e2c11eb64de43be14b6c1649790934f3
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-28 12:39:46 -0300

    refactor: Alterar href do css

--- Lista_Tarefas/Views/tarefas/editarTarefa.php
+++ Lista_Tarefas/Views/tarefas/editarTarefa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar Tarefa</title>
-    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../../public/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
