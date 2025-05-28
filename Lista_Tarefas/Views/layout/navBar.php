05482ae0f9cc4277df6fb279ef81f14ca0a0bf44
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-28 10:35:58 -0300

    fix: Erro nos botões home e nova tarefa

--- Lista_Tarefas/Views/layout/navBar.php
+++ Lista_Tarefas/Views/layout/navBar.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand ps-3" href="#">Lista de Tarefas</a>
+    <a class="navbar-brand ps-3" href="#">LISTA DE TAREFAS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="http://localhost:8080/dev-kabum/desafio-kabum/portal-administrativo/area-administrativa.php#">Área Administrativa</a>
+          <a class="nav-link active" aria-current="page" href="">HOME</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Cliente
+            FUNÇÕES
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="http://localhost:8080/dev-kabum/desafio-kabum/portal-administrativo/funcoes/cadastrarCliente.php#">Cadastrar</a></li>
+            <li><a class="dropdown-item" href="">Nova Tarefa</a></li>
           </ul>
         </li>
       </ul>
