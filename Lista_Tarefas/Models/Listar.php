2a8958d4c70a6ed7b4a648f8ecf7ab7dd92813c8
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-28 12:32:54 -0300

    feat: adicionar função listar tarefas por usuário com LEFT JOIN

---
+++ Lista_Tarefas/Models/Listar.php
@@ -0,0 +1,23 @@
+<?php
+    class Listar{
+        private PDO $database;
+
+        public function __construct(PDO $database) {
+            $this->database = $database;
+        }
+
+        public function listar(Tarefas $tarefas){
+            $sql = "SELECT id, titulo, descricao, dataCriacao AS Data_Criacao, usuario
+                    FROM listaTarefas
+                    LEFT JOIN login ON listaTarefas.id_usuario = login.idUsuario
+                    WHERE id_usuario = :id";
+            $params = [
+                "id" => $tarefas->getId()
+            ];
+            $stmt = $this->database->prepare($sql);
+            $stmt->execute($params);
+
+            $tarefas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+?>
