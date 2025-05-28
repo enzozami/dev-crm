a55c14625a5b0c62d1b689d8cbb614eb68a6602f
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-28 10:44:37 -0300

    feat(config): Criar connection

---
+++ Lista_Tarefas/config/connection.php
@@ -0,0 +1,17 @@
+<?php
+    class Database{
+        private string $serverName = "localhost"; 
+        private string $user = "root"; 
+        private string $password = "";
+        private string $dbName = "bancoCRM";
+    
+        public function conectar(): ? PDO {
+            try {
+                return new PDO("mysql:host={$this->serverName};dbname={$this->dbName};charset=utf8", $this->user, $this->password);
+            } catch (PDOException $ex) {
+                $errorMsg = "Erro ao tentar realizar conexão. ERRO: " . $ex->getMessage();
+                return null;
+            }
+        }
+    }
+?>
