b013fc1bd7bc0ec75802ee4b6cab36bd3d425833
Enzo Zamineli <enzozamineli@gmail.com>
2025-05-30 21:12:54 -0300

    feat(controller): implementar método listarTarefa e ajustes gerais
    - Criado método listarTarefa que instancia Tarefas com usuário da sessão e retorna listagem
    - Adicionado exit em função para controlar fluxo
    - Inicializado objeto Tarefas com valor null onde aplicável
    - Corrigido nome do campo POST de 'tarefa' para 'titulo'
    - Ajustada atribuição do usuário para usar (int) $_SESSION['idUsuario']
    - Incluídos require_once dos models Editar e Listar

--- Lista_Tarefas/controller/TarefaController.php
+++ Lista_Tarefas/controller/TarefaController.php
@@ -1,5 +1,7 @@
 <?php
     require_once "../models/Cadastrar.php";
+    require_once "../models/Editar.php";
+    require_once "../models/Listar.php";
     require_once "../models/Tarefas.php";
 
     class TarefaController{
@@ -12,22 +14,32 @@
         public function criarTarefa(){
             if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-                $titulo = $_POST['tarefa'];
+                $titulo = $_POST['titulo'];
                 $descricao = $_POST['descricao'];
                 $dataCriacao = $_POST['dataCriacao'];
-                $usuario = $_POST['usuario'];
+                $usuario = (int)$_SESSION['idUsuario'];
 
-                $tarefa = new Tarefas($titulo, $descricao, $dataCriacao, $usuario);
+                $tarefa = new Tarefas(null, $titulo, $descricao, $dataCriacao, $usuario);
 
                 $cadastro = new Cadastro($this->database);
                 $id = $cadastro->cadastrar($tarefa);
 
                 if($id){
                     header('Location: ../Views/dashboard.php');
+                    exit;
                 } else {
                     echo"<script> alert('Erro ao cadastrar tarefa!');</script>";
                 }
             }
+        }
+
+        public function listarTarefa(){
+            $usuario = (int) $_SESSION['idUsuario'];
+
+            $tarefas = new Tarefas(null, "", "", "", $usuario);
+            $listagem = new Listar($this->database);
+            $listar = $listagem->listar($tarefas);
+            return $listar;
         }
     }
 ?>
