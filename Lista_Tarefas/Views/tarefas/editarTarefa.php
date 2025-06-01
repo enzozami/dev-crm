b8be69d11b284e0d3d45e24b41b6b63f73541cec
Enzo Zamineli <enzozamineli@gmail.com>
2025-06-01 16:04:52 -0300

    refactor(editarTarefa): ajusta link do CSS e formata título principal em maiúsculas

--- Lista_Tarefas/Views/tarefas/editarTarefa.php
+++ Lista_Tarefas/Views/tarefas/editarTarefa.php
@@ -20,7 +20,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atualizar Tarefa</title>
-    <link rel="stylesheet" href="../../public/style.css">
+    <link rel="stylesheet" href="/dev-crm/Lista_Tarefas/public/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
